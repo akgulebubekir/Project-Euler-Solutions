1417201f12b73496ab9969d03a21d3eb362a37f6
akgulebubekir <ebubekirakgul91@gmail.com>
2016-06-07 23:59:15 +0300

    problem 50 solved

---
+++ pe50.py
@@ -0,0 +1,45 @@
+#-*- coding:utf-8 -*-
+
+"""
+The prime 41, can be written as the sum of six consecutive primes:
+
+41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most consecutive primes?
+"""
+import common
+import math
+
+primes = common.getPrimes(10**6)
+
+result, resCount = 0, 0
+
+def isPrime(num):
+	global primes
+	for i in primes:
+		if i == num:
+			return True
+		elif num%i == 0:
+			return False
+		elif i>math.sqrt(num):
+			return True
+	return True
+
+for i in range(0,len(primes)):
+	num = 0
+	count = 0
+	for j in primes[i:]:
+		count+=1
+		num +=j 
+
+		if num < 10**6 and isPrime(num) and resCount<count:
+			resCount = count
+			result = num
+		if num>10**6:
+			break
+
+print "result : "+ str(result)
+
